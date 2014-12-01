73fa184a881da89553e921b173b13c557ecb3970
Seitseman <romkoon@gmail.com>
2014-12-02 01:28:11 +0200

    1. Added functionality and tests to the Area class.

--- GeoServer/Area.h
+++ GeoServer/Area.h
@@ -7,6 +7,17 @@ class Area
 {
 public:
     Area(const Location& location, double width, double height);
+    Location upperLeft() const;
+    Location upperRight() const;
+    Location lowerRight() const;
+    Location lowerLeft() const;
+    bool inBounds(const Location& location) const;
+
+private:
+    double left_;
+    double right_;
+    double top_;
+    double bottom_;
 };
 
 #endif // AREA_H
--- GeoServer/AreaTest.cpp
+++ GeoServer/AreaTest.cpp
@@ -1,1 +1,100 @@
 #include "gmock/gmock.h"
+
+#include "Area.h"
+
+using namespace std;
+using namespace ::testing;
+
+class AnArea : public Test
+{
+public:
+
+    const double Tolerance {0.005};
+    const Location Zero{0,0};
+    const double OneKm{1000};
+    const double HalfOneKm{500};
+    const double TwoKm{2000};
+    const double HalfTwoKm{1000};
+    const Area OneByTwoAreaAtZero{Zero, OneKm, TwoKm};
+};
+
+
+TEST_F(AnArea, AnswersUpperleftForBoundingRectangle)
+{
+    Location upperLeft{OneByTwoAreaAtZero.upperLeft()};
+
+    ASSERT_NEAR(Zero.go(HalfOneKm, West).longitude(), upperLeft.longitude(), Tolerance);
+    ASSERT_NEAR(Zero.go(HalfTwoKm, North).latitude(), upperLeft.latitude(), Tolerance);
+}
+
+TEST_F(AnArea, AnswersUpperRightForBoundingRectangle)
+{
+    Location upperRight{OneByTwoAreaAtZero.upperRight()};
+
+    ASSERT_NEAR(Zero.go(HalfOneKm, East).longitude(), upperRight.longitude(), Tolerance);
+    ASSERT_NEAR(Zero.go(HalfTwoKm, North).latitude(), upperRight.latitude(), Tolerance);
+}
+
+TEST_F(AnArea, AnswersLowerRightForBoundingRectangle)
+{
+    Location lowerRight{OneByTwoAreaAtZero.lowerRight()};
+
+    ASSERT_NEAR(Zero.go(HalfOneKm, East).longitude(), lowerRight.longitude(), Tolerance);
+    ASSERT_NEAR(Zero.go(HalfTwoKm, South).latitude(), lowerRight.latitude(), Tolerance);
+}
+
+TEST_F(AnArea, AnswersLowerLeftForBoundingRectangle)
+{
+    Location lowerLeft{OneByTwoAreaAtZero.lowerLeft()};
+
+    ASSERT_NEAR(Zero.go(HalfOneKm, West).longitude(), lowerLeft.longitude(), Tolerance);
+    ASSERT_NEAR(Zero.go(HalfTwoKm, South).latitude(), lowerLeft.latitude(), Tolerance);
+}
+
+TEST_F(AnArea, DesignatesAPointOutOfBoundsWhenTooFarNorth)
+{
+    Location tooNorth{Zero.go(HalfTwoKm + 10, North).latitude(), 0};
+    ASSERT_FALSE(OneByTwoAreaAtZero.inBounds(tooNorth));
+}
+
+TEST_F(AnArea, DesignatesAPointInBoundsWhenAtNorth)
+{
+    Location northEdge{Zero.go(HalfTwoKm, North).latitude(), 0};
+    ASSERT_TRUE(OneByTwoAreaAtZero.inBounds(northEdge));
+}
+
+TEST_F(AnArea, DesignatesAPointoutOfBoundsWhenTooFarSouth)
+{
+    Location tooSouth{Zero.go(HalfTwoKm + 10, South).latitude(), 0};
+    ASSERT_FALSE(OneByTwoAreaAtZero.inBounds(tooSouth));
+}
+
+TEST_F(AnArea, DesignatesAPointInBoundsWhenAtSouth)
+{
+    Location southEdge{Zero.go(HalfTwoKm, South).latitude(), 0};
+    ASSERT_TRUE(OneByTwoAreaAtZero.inBounds(southEdge));
+}
+
+TEST_F(AnArea, DesignatesAPointOutOfBoundsWhenTooFarWest)
+{
+    Location tooWest{0, Zero.go(HalfOneKm + 10, West).longitude()};
+    ASSERT_FALSE(OneByTwoAreaAtZero.inBounds(tooWest));
+}
+
+TEST_F(AnArea, DesignatesAPointInBoundsWhenAtWest)
+{
+    Location westEdge{0, Zero.go(HalfOneKm, West).longitude()};
+    ASSERT_TRUE(OneByTwoAreaAtZero.inBounds(westEdge));
+}
+
+TEST_F(AnArea, DesignatesAPointOutOfBoundsWhenTooFarEast)
+{
+    Location tooEast{0, Zero.go(HalfOneKm + 10, East).longitude()};
+    ASSERT_FALSE(OneByTwoAreaAtZero.inBounds(tooEast));
+}
+
+TEST_F(AnArea, DesignatesAPointInBoundsWhenAtEast)
+{
+    Location eastEdge{0, Zero.go(HalfOneKm, East).longitude()};
+    ASSERT_TRUE(OneByTwoAreaAtZero.inBounds(eastEdge));
+}
