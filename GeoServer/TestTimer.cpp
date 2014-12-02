c5a909aaffb9ba7ac0c3bebec7bf9e3800359e17
Seitseman <romkoon@gmail.com>
2014-12-02 03:19:31 +0200

    1. Added Worker and ThreadPool files.

---
+++ GeoServer/TestTimer.cpp
@@ -0,0 +1,5 @@
+#include "TestTimer.h"
+
+TestTimer::TestTimer()
+{
+}
---
+++ GeoServer/TestTimer.h
@@ -0,0 +1,10 @@
+#ifndef TESTTIMER_H
+#define TESTTIMER_H
+
+class TestTimer
+{
+public:
+    TestTimer();
+};
+
+#endif // TESTTIMER_H
