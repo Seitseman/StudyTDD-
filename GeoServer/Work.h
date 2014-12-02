3df875d2e874880b2410a5b45d9192f29ba38eb7
Seitseman <romkoon@gmail.com>
2014-12-02 03:20:00 +0200

    1. Implemented and tested Work class.

--- GeoServer/Work.h
+++ GeoServer/Work.h
@@ -1,4 +1,29 @@
 #ifndef WORK_H
 #define WORK_H
 
+#include <functional>
+
+class Work
+{
+public:
+    static const int DefaultId{0};
+    Work(int id = DefaultId)
+        : id_{id}
+        , executeFunction_{[]{}} {}
+    Work(std::function<void()> executeFunction, int id = DefaultId)
+        : id_{id}
+        , executeFunction_{executeFunction}
+    {}
+
+    void execute() {
+        executeFunction_();
+    }
+
+    int id() const { return id_; }
+
+private:
+    int id_;
+    std::function<void()> executeFunction_;
+};
+
 #endif // WORK_H
--- GeoServer/WorkTest.cpp
+++ GeoServer/WorkTest.cpp
@@ -1,1 +1,87 @@
+#include "Work.h"
 
+#include "gmock/gmock.h"
+#include <functional>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+using namespace ::testing;
+
+class AWorkObject : public Test
+{
+};
+
+
+TEST_F(AWorkObject, DefaultsFunctionToNullObject)
+{
+    Work work;
+    try {
+        work.execute();
+    }
+    catch (...) {
+        FAIL() <<"unable to execute function";
+    }
+}
+
+TEST_F(AWorkObject,  DefaultsFunctionToNullObjectWhenConstructedWithId)
+{
+    Work work(1);
+    try {
+        work.execute();
+    }
+    catch(...) {
+        FAIL() << "unable to execute function";
+    }
+}
+
+TEST_F(AWorkObject, CanbeConstructedWithAnID)
+{
+    Work work(1);
+    ASSERT_EQ(1, work.id());
+}
+
+TEST_F(AWorkObject, Defaults_ID_To_Zero)
+{
+    Work work;
+    ASSERT_EQ(0, work.id());
+}
+
+TEST_F(AWorkObject, Defaults_id_to_0_when_function_specified)
+{
+    Work work{[]{}};
+    ASSERT_EQ(0, work.id());
+}
+
+TEST_F(AWorkObject, Can_be_constructed_With_a_function_and_ID)
+{
+    Work work{[]{}, 1};
+    ASSERT_EQ(1, work.id());
+}
+
+TEST_F(AWorkObject, ExecutesFunctionStored)
+{
+    bool wasExecuted{false};
+    auto executeFunction = [&](){wasExecuted = true;};
+    Work work(executeFunction);
+    work.execute();
+    ASSERT_TRUE(wasExecuted);
+}
+
+TEST_F(AWorkObject, Can_execute_on_data_captured_with_function)
+{
+    vector<string> data{"a", "b"};
+    string result;
+    auto callBackFunction = [&](string s) {
+        result.append(s);
+    };
+
+    auto executFunction = [&]() {
+        stringstream s;
+        s << data[0] << data[1];
+        callBackFunction(s.str());
+    };
+    Work work(executFunction);
+    work.execute();
+    ASSERT_EQ("ab", result);
+}
